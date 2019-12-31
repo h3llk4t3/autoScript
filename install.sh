7b1a9f3d53ea23f18e6ef7b45f54d6c35ca30314
Treebug842 <51908187+Treebug842@users.noreply.github.com>
2020-01-01 04:08:40 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,25 +19,11 @@ git clone https://github.com/ZerBea/hcxtools.git
 cd hcxtools
 make
 make install
-cd ~
-touch autoScript
-echo "#!/bin/bash" >> autoScript
-echo "INPUT=${1}" >> autoScript
-echo "if [ -z $INPUT ]" >> autoScript
-echo "then" >> autoScript
-echo "cd ~/Programs/autoScript" >> autoScript
-echo "./autoScript.sh" >> autoScript
-echo "elif [ $INPUT == update ]" >> autoScript
-echo "then" >> autoScript
-echo "cd ~/Programs/autoScript" >> autoScript
-echo "./update" >> autoScript
-echo "else" >> autoScript
-echo "echo "Not a modifier"" >> autoScript
-echo "fi" >> autoScript
+cd ~/Programs/autoScript
+
 chmod +x autoScript
 sudo mv autoScript /bin/
 
-cd ~/Programs/autoScript/
 mkdir Handshake
 mkdir PMKID
 
